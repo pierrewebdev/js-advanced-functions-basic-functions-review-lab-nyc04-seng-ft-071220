4afe33ddfed727ac75e1d0939af48598c014fe06
Patrick Pierre <patrick.pierre000@gmail.com>
2020-10-05 13:03:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+
+//example of hosting
+function hoistingEx(){
+  console.log(myThoughts);
+  console.log("waiting");
+  console.log("waiting");
+  
+  var myThoughts;
+  myThoughts = "I like PHP";
+  
+  // console.log(`When it comes to programming languages I think ${myThoughts}`);
+}
+
+hoistingEx()
